79f0299ac170a369018954465990c0287c21a41f
Katie O'Mahony <61972958+aforestsomewhere@users.noreply.github.com>
2025-07-23 16:40:49 +0100

    Update setup_folders.sh

--- setup_folders.sh
+++ setup_folders.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Convert any .gbff files to .gbk for standardisation
+for file in *.gbk; do
+  filename="${file%.gbff}"
+  python convert_gbff_gbk.py "$filename.gbff" "$filename.gbk"
+done
+
 # Loop through each .gbk file in the current directory
 for file in *.gbk; do
-  # Remove the .gbff suffix from the filename to get the directory name
+  # Remove the .gbk suffix from the filename to get the directory name
   dirname="${file%.gbk}"
-  
   # Create the directory
   mkdir -p "$dirname"
   
@@ -13,3 +18,6 @@ for file in *.gbk; do
   #ln -s "$dirname"/"$dirname".gbff "$dirname".gbff
   mv *"$dirname"* "$dirname"
 done
+
+#Add db_xref taxon id information to .gbk files
+python add_dbxref.py
